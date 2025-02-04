81d637e7e9ac92bcf371c2a7648a1adc87a45bca
Federica <fede.duraccio@gmail.com>
2025-02-04 22:23:55 +0100

    FIX: minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { EducationComponent } from './pages/education/education.component';
-import { ExperienceComponent } from './pages/experience/experience.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { SkillCardComponent } from './components/skill-card/skill-card.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { EducationComponent } from "./pages/education/education.component";
+import { ExperienceComponent } from "./pages/experience/experience.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatCardModule } from "@angular/material/card";
+import { SkillCardComponent } from "./components/skill-card/skill-card.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +23,8 @@ import { SkillCardComponent } from './components/skill-card/skill-card.component
     ExperienceComponent,
     NavbarComponent,
     CarouselComponent,
-    SkillCardComponent  ],
+    SkillCardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,9 +32,9 @@ import { SkillCardComponent } from './components/skill-card/skill-card.component
     MatToolbarModule,
     MatIconModule,
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
